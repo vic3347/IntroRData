a0613cfe9461d9debf0d7a6fe6019a3a68dda967
Victor Thompson <73864991+vic3347@users.noreply.github.com>
2025-02-11 22:43:46 -0500

    adding data

--- intro-r-data-analysis.R
+++ intro-r-data-analysis.R
@@ -3,3 +3,29 @@
 # vthompson@rider.edu
 # Feb 12, 2025
 
+
+dir.create(path = "data")
+dir.create(path = "output")
+
+# This command loads a preloaded dataset from R.
+head(x = iris)
+
+# Boxplot for each species
+boxplot(formula = Petal.Length ~ Species, data = iris)
+
+# Check the dimensions
+dim(iris)
+
+# Create new variables
+setosa <- iris[iris$Species == "setosa", ]
+versicolor <- iris[iris$Species == "versicolor", ]
+
+# Comparing different data objects
+dim(iris)
+dim(setosa)
+dim(versicolor)
+
+nrow(iris)
+nrow (setosa)
+
+
